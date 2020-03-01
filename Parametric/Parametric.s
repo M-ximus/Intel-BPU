479d78acfa4da61b69bc7c1dd5481bdce0e1e5b7
Maxim Davydov <Davydov.MA@phystech.edu>
2020-03-01 23:24:01 +0300

    Table with comments about each test

--- Parametric/Parametric.s
+++ Parametric/Parametric.s
@@ -14,12 +14,12 @@ main_loop:
 br_taken:
 
 # smth like for()
-
+    nop # we need this nop for small parameters for history of two-level predictor
     addi t0, t0, -1
     bnez t0, br_taken
 
 # loop end
-
+    nop
     nop
 
     j main_loop # Direct Unconditional Backward branch
